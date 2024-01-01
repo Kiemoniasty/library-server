265938a3dd2f7ad42d88523558b9420ed48ca420
Wojciech Zduńczyk <wojtek.zdunczyk@gmail.com>
2024-01-01 18:34:37 +0100

    Added Reservations controller

--- Library_WebServer/Controllers/ReservationsController.cs
+++ Library_WebServer/Controllers/ReservationsController.cs
@@ -5,55 +5,55 @@ namespace Library_WebServer.Controllers
 {
     [ApiController]
     [Route("[controller]/")]
-    public class CommentsController : ControllerBase
+    public class ReservationsController : ControllerBase
     {
-        private readonly ILogger<CommentsController> _logger;
+        private readonly ILogger<ReservationsController> _logger;
 
-        public CommentsController(ILogger<CommentsController> logger)
+        public ReservationsController(ILogger<ReservationsController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        [Route("{commentId}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryComment))]
+        [Route("{reservationId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryReservation))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult GetComment([FromRoute] Guid commentId)
+        public IActionResult GetReservation([FromRoute] Guid reservationId)
         {
             return Ok();
         }
 
         [HttpPost]
         [Route("")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryComment))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryReservation))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult PostComment([FromBody] LibraryComment comment)
+        public IActionResult PostReservation([FromBody] LibraryReservation reservation)
         {
             return Ok();
         }
 
         [HttpPut]
         [Route("")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryComment))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryReservation))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult PutComment([FromBody] LibraryComment comment)
+        public IActionResult PutReservation([FromBody] LibraryReservation reservation)
         {
             return Ok();
         }
 
         [HttpDelete]
-        [Route("{publicationId}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryComment))]
+        [Route("{reservationId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibraryReservation))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult DeleteComment([FromRoute] Guid commentId)
+        public IActionResult DeleteReservation([FromRoute] Guid reservationId)
         {
             return Ok();
         }
