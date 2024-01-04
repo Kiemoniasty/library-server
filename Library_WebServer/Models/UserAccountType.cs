aeec489759e509def19de169411a3f256724d733
Wojciech Zduńczyk <wojtek.zdunczyk@gmail.com>
2024-01-04 18:42:11 +0100

    Added basic post logic for user

--- Library_WebServer/Models/UserAccountType.cs
+++ Library_WebServer/Models/UserAccountType.cs
@@ -1,10 +1,15 @@
 ﻿using Library_WebServer.Enums;
+using System.Text.Json.Serialization;
 
 namespace Library_WebServer.Models;
 
 public class UserAccountType : EnumTable<UserAccountTypeEnum>
 {
     public UserAccountType(UserAccountTypeEnum @enum) : base(@enum)
+    { }
+
+    [JsonConstructor]
+    public UserAccountType(UserAccountTypeEnum id, string name) : base(id, name)
     { }
 
     protected UserAccountType() { }
