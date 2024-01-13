be0fd04875dd7d2d0b12317f411118a8ac377359
BartłomiejSwit <switbartlomiej@gmail.com>
2024-01-14 00:39:45 +0100

    Login-wait-for-check-by-Zdunalke

---
+++ Library_WebServer/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using Library_WebServer.Database;
+using Library_WebServer.Models.Database;
+using Library_WebServer.Models.Requests.Comment;
+using Library_WebServer.Models.Requests.Login;
+using Library_WebServer.Models.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_WebServer.Controllers
+{
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+        private readonly LibraryDbContext _libraryDbContext;
+
+        public LoginController(ILogger<LoginController> logger, LibraryDbContext libraryDbContext)
+        {
+            _logger = logger;
+            _libraryDbContext = libraryDbContext;
+        }
+
+        [HttpPost]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult PostLogin([FromBody] LoginRequestBaseModel login)
+        {
+            UserDbModel? loginDB = _libraryDbContext.Users
+                .Include(x => x.UserAccountType)
+                .Include(x => x.LibraryRentals)
+                    .ThenInclude(x => x.LibraryPublication)
+                .SingleOrDefault(x => x.Name == login.Name);
+
+            bool pass = true;
+
+            if (loginDB == null)
+            {
+                pass = false;
+                return Ok(pass);
+            }
+
+            if (loginDB.Password != login.Password) 
+            {
+                pass = false;
+                return Ok(pass);
+            }
+
+            return Ok(pass);
+        }
+    }
+}
---
+++ Library_WebServer/Models/Requests/Login/LoginRequestBaseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Library_WebServer.Models.Requests.Login
+{
+    public class LoginRequestBaseModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public LoginRequestBaseModel() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public LoginRequestBaseModel(
+            string firstName,
+            string lastName)
+        {
+            Name = firstName;
+            Password = lastName;
+        }
+    }
+}
