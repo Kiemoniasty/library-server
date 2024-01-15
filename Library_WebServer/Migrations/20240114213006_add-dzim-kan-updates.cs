7fa18d307751e46ebd1c68a19bb26926509b5920
BartłomiejSwit <switbartlomiej@gmail.com>
2024-01-15 02:52:33 +0100

    changes-for-dziem-kan-3-tested-end

---
+++ Library_WebServer/Migrations/20240114213006_add-dzim-kan-updates.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library_WebServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddzimkanupdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
